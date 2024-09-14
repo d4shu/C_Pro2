a91e3566733b9bdcb907ee7091fd82b0f4db02bd
d4shu <d4shu05@gmail.com>
2024-09-14 14:33:10 +0900

    3주차_포인터와 배열

---
+++ 3주차(동영상) 실습_포인터와 배열 실습.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 100
+
+void inputRandomArray(int* array, int size){
+    for(int i=0; i<SIZE; i++){
+        *(array+i) = rand() % 100;         // array[i] <-> *(array + i)
+    }
+}
+void printArray(int* array, int size)
+{
+    for(int i=0; i<size; i++){
+        printf("%3d\t", array[i]);
+        if((i+1) % 10 == 0) printf("\n");
+    }
+}
+
+
+
+int main()
+{
+    int number[SIZE];
+    
+    srand(time(NULL));
+    inputRandomArray(number, SIZE);
+    printArray(number, SIZE);
+    
+    return 0;
+}
