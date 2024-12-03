8a4d98d04ed2ec7158fc5b8df3f297b2559af93e
d4shu <d4shu05@gmail.com>
2024-12-03 21:28:17 +0900

    Update week 12-3) 시간처리.c

--- week 12-3) 시간처리.c
+++ week 12-3) 시간처리.c
@@ -1,6 +1,6 @@
 //입력한 날짜의 해당 요일을 출력 / 입력한 날짜가 오늘로부터 며칠 남았는지(전인지) 출력
 
-void_exec1(){
+/*
     
 #include <stdio.h>
 #include <time.h>
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]){
     int year, month, day;
     char* WEEK[] = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
 
-    printf("Enter YYY MM DD : ");
+    printf("Enter YYYY MM DD : ");
     scanf("%d %d %d", &year, &month, &day);
 
     current = time(NULL);
@@ -31,9 +31,8 @@ int main(int argc, char* argv[]){
 
 }
 
-}
+*/
 
-void_exec2(){
     
 #include <stdio.h>
 #include <time.h>
@@ -43,4 +42,4 @@ int main(int argc, char* argv[]){
     week2(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
 }
 
-}
+
